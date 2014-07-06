f546a03b86686db84a590068fc48736771647267
supersheep <supersheepxjy@gmail.com>
2014-07-06 21:14:45 +0800

    better ui, and support browserify specs

---
+++ README.md
@@ -0,0 +1,8 @@
+#Cortex Migrate
+
+
+migrate npm module to cortex registry.
+
+## Basic usage
+
+  cortex migrate <package>[@<version>]
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 var fs = require('fs');
-var spawn = require('child_process').spawn;
 var Migrator = require('./lib/');
 
 var migrator = new Migrator("/Users/spud/Product/neurons/temp-neurons");
-migrator.migrate("socket.io-client","latest", function(){
-  console.log("wulala!",arguments);
+
+var module = process.argv[2].split("@");
+
+migrator.migrate(module[0], module[1] || "latest", function(err){
+  if(err){
+    console.log("ERR!".red, err.message);
+  }else{
+    console.log("\ndone!".green);
+  }
 });
